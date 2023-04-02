16e0a2a3f2283b3bff0c6fe6491b7bc92ee15c87
Pavel Sergeev <sergeevpasha90@gmail.com>
2023-04-02 16:53:59 +0700

    chore: lint fix

--- pages/api/users/[address].ts
+++ pages/api/users/[address].ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import UserService from '../../../app/services/UserService';
 import { UserModel } from '../../../app/models/User';
-import dbConnect from "../../../mongoose";
+import dbConnect from '../../../mongoose';
 
 async function showUser(address: string, response: NextApiResponse): Promise<void> {
     const user: UserModel | null = await UserService.findByAddress(address);
